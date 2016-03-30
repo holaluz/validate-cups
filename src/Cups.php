42a62c31b78fbef75008f168c9abda3193d76955
Xavier Mabras <xavier.mabras@holaluz.com>
2016-03-30 23:25:14 +0200

    Add a bit of Documentation

--- src/Cups.php
+++ src/Cups.php
@@ -2,8 +2,24 @@
 
 namespace CupsValidate;
 
+/**
+ * Class Cups
+ *
+ * El CUPS (Código Unificado del Punto de Suministro), en España, es un código 
+ * único e identificador de un punto de suministro de energía, ya sea de electricidad
+ * o gas canalizado.
+ *
+ * @link https://es.wikipedia.org/wiki/C%C3%B3digo_Unificado_de_Punto_de_Suministro
+ */
 class Cups
 {
+    /**
+     * CUPS validation.
+     *
+     * @param string $cups The CUPS
+     *
+     * @return bool
+     */
     public static function validate($cups = true)
     {
         return $cups;
