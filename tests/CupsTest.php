064ca7d5db43c7950d311d0b0ebed2aa00293b68
Xavier Mabras <xavier.mabras@holaluz.com>
2016-04-01 09:38:56 +0200

    Remove unused files

--- tests/CupsTest.php
+++ tests/CupsTest.php
@@ -2,8 +2,8 @@
 
 use CupsValidate/Cups;
 
-class CupsTest extends PHPUnit_Framework_TestCase {
-
+class CupsTest extends PHPUnit_Framework_TestCase
+{
     public function testCupsHasTrue()
     {
         $this->assertTrue(Cups::validate());
